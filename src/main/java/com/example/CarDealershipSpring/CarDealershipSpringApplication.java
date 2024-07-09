763386d2d674f2f4b4b761c3464d5d9ade8a0d59
noahbeito <noah.beito@cvent.com>
2024-07-09 10:32:45 -0700

    Spring initialized project

---
+++ src/main/java/com/example/CarDealershipSpring/CarDealershipSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarDealershipSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarDealershipSpring/CarDealershipSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarDealershipSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
